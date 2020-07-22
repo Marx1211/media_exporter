c74b73f6c2e2084037138cfe2e822e3f316bbf67
Marcos De Faria Perez <mdfp1211@gmail.com>
2020-07-22 11:19:01 +0200

    Added script to get sd card

---
+++ card_reader.py
@@ -0,0 +1,16 @@
+from string import ascii_uppercase
+import os
+
+def get_card():
+    for sd_path in ascii_uppercase:
+        if os.path.exists('%s:\\Fuji.ID' % SVPATH):
+            sd_path='%s:\\' % sd_path
+            print('SD card mounted to ', sd_path)
+            return sd_path + ""
+    return ""
+
+sd_card = get_card()
+while sd_card == "":
+    print('Please plug in SD card and press any key to continue...', end="")
+    input()
+    sd_card = get_card()
