a6c5907b6397270ee0bedb13a77598fd4a1e2856
Marcos De Faria Perez <mdfp1211@gmail.com>
2020-07-23 19:21:50 +0200

    Added methods to set destination path and move files.

--- card_reader.py
+++ card_reader.py
@@ -1,5 +1,6 @@
 from string import ascii_uppercase
 import os
+import shutil
 
 def get_card():
     for sd_path in ascii_uppercase:
@@ -9,8 +10,20 @@ def get_card():
             return sd_path + ""
     return ""
 
+def set_destination():
+    destination_path = ""
+    while destination_path == "":
+        destination_path = input('Write the folder name where you want the files to go: ')
+        return destination_path + ""
+
+def move_files(destination):
+    shutil.move(sd_card + "\\DCIM\\741_FUJI", destination)
+
+
 sd_card = get_card()
 while sd_card == "":
     print('Please plug in SD card and press any key to continue...', end="")
     input()
     sd_card = get_card()
+dest = set_destination()
+move_files(dest)
