3deef262f4054f0e72cd56b50f7080e819844298
Marcos De Faria Perez <mdfp1211@gmail.com>
2020-07-24 21:25:15 +0200

    Wrote function to get dates from files in card. Only displays dates.

--- card_reader.py
+++ card_reader.py
@@ -1,24 +1,28 @@
 from string import ascii_uppercase
 import os
 import shutil
+from datetime import datetime
 
 def get_card():
     for sd_path in ascii_uppercase:
         if os.path.exists('%s:\\Fuji.ID' % sd_path):
             sd_path='%s:\\' % sd_path
             print('SD card mounted to ', sd_path)
-            return sd_path + ""
+            return sd_path + "DCIM\\741_FUJI\\"
     return ""
 
 def set_destination():
     destination_path = ""
     while destination_path == "":
         destination_path = input('Write the folder name where you want the files to go: ')
-        return destination_path + ""
+        return "D:\\" + destination_path + ""
 
-def move_files(destination):
-    shutil.move(sd_card + "\\DCIM\\741_FUJI", destination)
-
+def move_by_date(origin):
+    pics = os.listdir(origin)
+    for picture in pics:
+        if(picture.endswith('.RAF')):
+            pic_date = os.path.getctime(origin + picture)
+            print(datetime.fromtimestamp(pic_date))
 
 sd_card = get_card()
 while sd_card == "":
@@ -26,4 +30,4 @@ while sd_card == "":
     input()
     sd_card = get_card()
 dest = set_destination()
-move_files(dest)
+move_by_date(sd_card)
